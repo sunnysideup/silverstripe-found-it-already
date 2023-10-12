ce1a0ef16cd403db397b5d17ec6d6cabcb143c38
Nicolaas <github@sunnysideup.co.nz>
2023-10-12 14:25:03 +1300

    WIP

---
+++ app/app/src/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+use SilverStripe\CMS\Controllers\ContentController;
+
+/**
+ * Class \PageController.
+ *
+ * @property Page $dataRecord
+ * @method Page data()
+ * @mixin PageControllerExtension
+ */
+class PageController extends ContentController
+{
+}
