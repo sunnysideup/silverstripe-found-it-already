74bbddd17c87d7d70902a80604fdd3d13efd9472
Nicolaas <github@sunnysideup.co.nz>
2024-01-17 14:15:06 +1300

    MNT

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -11,4 +11,11 @@ use SilverStripe\CMS\Controllers\ContentController;
  */
 class PageController extends ContentController
 {
+    protected function init()
+    {
+        parent::init();
+        // Add any extra CSS or JS files you want to be included on all pages
+        // Requirements::css('mytheme/css/layout.css');
+        // Requirements::javascript('mytheme/javascript/layout.js');
+    }
 }
