c1a28ce300fae007aecd86dd70728e9ba740c989
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-04-20 08:25:10 +1200

    FIX: cleanup

--- bin/release-test.sh
+++ bin/release-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+vendor/bin/sake dev/tasks/migrations
 vendor/bin/sake-release
+vendor/bin/sake dev/tasks/migrations
 vendor/bin/sake dev/tasks/Firesphere-CSPHeaders-Tasks-SRIRefreshTask
